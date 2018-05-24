76b73a9762a98631ed696f0c4b22f2945309b21b
Paul Bomba <paul@paulbomba.com>
2018-05-24 02:54:45 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,10 @@
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
-    return fizzbuzz
+    return "fizzbuzz"
   elsif num % 3 == 0
-  
+    return "fizz"
+  elsif num % 5
+    return "buzz"
+  else
+    return nil
 end
